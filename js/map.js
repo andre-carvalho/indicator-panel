aea34f3fe649c6269f1ee6d48392cfcc391bcafc
André Fernando Araújo de Carvalho <afacarvalho@yahoo.com.br>
2023-09-13 13:51:53 -0300

    Update map.js
    Change the base map source to openstreetmap.org example

--- js/map.js
+++ js/map.js
@@ -32,12 +32,9 @@ var mainMap={
         }
         mainMap.map = L.map('mainmap').setView([-23, -45], mainMap.defaultZoomLevel);
 
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 18,
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            id: 'mapbox/light-v9',
-            tileSize: 512,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
             zoomOffset: -1
         }).addTo(mainMap.map);
 
